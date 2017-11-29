aa43cfaa2cc5161dbea81c418fdb1ead3072f49a
dorr <dor.raba@gmail.com>
2017-11-29 10:12:51 +0200

    add redux dev tools

--- configureStore.js
+++ configureStore.js
@@ -2,8 +2,9 @@
 
 import { createStore } from 'redux'
 import rootReducer from './reducers'
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 export default function configureStore() {
-  let store = createStore(rootReducer)
+  let store = createStore(rootReducer, composeWithDevTools())
   return store
 }
