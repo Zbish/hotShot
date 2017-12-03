705b57774a47bd7876bbe40a15b3add5718b7e7d
Omri Doron <omridoron3@gmail.com>
2017-12-03 20:42:42 +0200

    teams code

--- src/screens/addLiga.js
+++ src/screens/addLiga.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
-import {Text,
+import {
+  Text,
   View,
   Button,
   ImageBackground,
-  TextInput} from 'react-native'
+  TextInput
+} from 'react-native'
 import { connect } from 'react-redux';
-import { addNewLeague, addPlayerToNewLeague, addGameToNewLeague, changeNameOfNewLeague } from '../actions';
+import {
+  addNewLeague,
+  addPlayerToNewLeague,
+  addGameToNewLeague,
+  changeNameOfNewLeague
+} from '../actions';
 import AddFriend from '../components/AddFriends'
 import AddGames from '../components/AddGames'
 import styles from './addLiga.style';
 
- class AddLeague extends Component {
-  addLiga()
-  {
+class AddLeague extends Component {
+  addLiga() {
     this.props.addNewLeague();
     this.props.navigation.navigate("MyLiga");
   }
-  addPlayer(player){
+  addPlayer(player) {
     this.props.addPlayer(player);
   }
-  addGame(game){
+  addGame(game) {
     this.props.addGame(game);
   }
   onChange(name) {
@@ -31,33 +37,36 @@ import styles from './addLiga.style';
       <ImageBackground source={require('../images/app/field.jpg')} style={styles.wrapper}>
         <View style={styles.container}>
           <View style={styles.sub1}>
-          <TextInput underLineColorAndroid='transparent'
-                   placeholderTextColor="black"
-                   placeholder='league name'
-                   style={styles.textInput}
-                   onChangeText={(val) => this.onChange(val)}
-                   value={this.props.leagues.newLeague.name}
-        />
+            <TextInput underLineColorAndroid='transparent'
+              placeholderTextColor="black"
+              placeholder='league name'
+              style={styles.textInput}
+              onChangeText={(val) => this.onChange(val)}
+              value={this.props.leagues.newLeague.name}
+            />
           </View>
-       <View style={styles.sub2}>
-       <AddFriend friends={this.props.friends} addPlayer={(name)=>this.addPlayer(name)} />
+          <View style={styles.sub2}>
+            <AddFriend friends={this.props.friends}
+              addPlayer={(name) => this.addPlayer(name)} />
             <View style={styles.friendsInLiga}>
               <Text style={styles.text}>Players :</Text>
-                    {
-                    this.props.leagues.newLeague.players.map((item, index) => {
-                                    return <Text style={styles.text} key={index} item={item}>{item.name},</Text>
-                  })
-                }
+              {
+                this.props.leagues.newLeague.players.map((item, index) => {
+                  return <Text style={styles.text}
+                    key={index}
+                    item={item}>{item.name},</Text>
+                })
+              }
             </View>
-       </View>
-         <View style={styles.sub3}>
-         <View style={styles.gamesInLiga}>
+          </View>
+          <View style={styles.sub3}>
+            <View style={styles.gamesInLiga}>
               <Text style={styles.text}>Games : {this.props.leagues.newLeague.games.length}</Text>
             </View>
-         <AddGames style={styles.games} games={this.props.games} teams={this.props.team} addGame={(game) => this.addGame(game)} />
-         </View>
+            <AddGames style={styles.games} games={this.props.games} teams={this.props.team} addGame={(game) => this.addGame(game)} />
+          </View>
 
-            <Button title={'add league'} onPress={()=>this.addLiga()} ></Button>
+          <Button title={'add league'} onPress={() => this.addLiga()} ></Button>
         </View>
 
       </ImageBackground>
@@ -66,25 +75,25 @@ import styles from './addLiga.style';
 }
 
 
-  function mapStateToProps (state) {
-    return {
-      leagues: state.leagues,
-      games:state.games,
-      team:state.team,
-      friends: state.friends
-    }
+function mapStateToProps(state) {
+  return {
+    leagues: state.leagues,
+    games: state.games,
+    team: state.team,
+    friends: state.friends
   }
+}
 
-  function mapDispatchToProps (dispatch) {
-    return {
-      addNewLeague: (league) => dispatch(addNewLeague(league)),
-      addPlayer: (name) => dispatch(addPlayerToNewLeague(name)),
-      addGame: (game) => dispatch(addGameToNewLeague(game)),
-      changeName: (name)  => dispatch(changeNameOfNewLeague(name))
-    }
+function mapDispatchToProps(dispatch) {
+  return {
+    addNewLeague: (league) => dispatch(addNewLeague(league)),
+    addPlayer: (name) => dispatch(addPlayerToNewLeague(name)),
+    addGame: (game) => dispatch(addGameToNewLeague(game)),
+    changeName: (name) => dispatch(changeNameOfNewLeague(name))
   }
+}
 
-  export default connect(
-    mapStateToProps,
-    mapDispatchToProps,
-  )(AddLeague)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(AddLeague)
